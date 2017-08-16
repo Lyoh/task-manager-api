a61f00bcdb6ee06339d468a2f7b7c05f222e557e
Riubbe Oliveira <rb.oliveira@hotmail.com>
2017-08-16 13:56:29 +0000

    users tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before { @user = FactoryGirl.build(:user) }
+  # example 1
+  # before { @user = FactoryGirl.build(:user) }
 
-  it { expect(@user).to respond_to(:email) }
-  it { expect(@user).to respond_to(:name) }
-  it { expect(@user).to respond_to(:password) }
-  it { expect(@user).to respond_to(:password_confirmation) }
-  it { expect(@user).to be_valid }
+  # it { expect(@user).to respond_to(:email) }
+  # it { expect(@user).to respond_to(:name) }
+  # it { expect(@user).to respond_to(:password) }
+  # it { expect(@user).to respond_to(:password_confirmation) }
+  # it { expect(@user).to be_valid }
+
+  # example 2
+  # subject { build(:user) }
+
+  # it { expect(subject).to respond_to(:email) }
+  # it { expect(subject).to be_valid }
+
+  # example 3
+  it { is_expected.to respond_to(:email) }
+  it { is_expected.to respond_to(:name) }
 end
