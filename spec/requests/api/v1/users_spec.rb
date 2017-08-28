553be4d2f959c20930426cc6426dc8d25770a31f
Riubbe Oliveira <rb.oliveira@hotmail.com>
2017-08-28 00:59:50 +0000

    Refactoring the tests

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -3,31 +3,29 @@ require 'rails_helper'
 RSpec.describe 'Users API', type: :request do
     let!(:user) { create(:user) }
     let(:user_id) { user.id }
+    let(:headers) do
+        {
+            'Accept' => 'application/vnd.taskmanager.v1',
+            'Content-Type' => Mime[:json].to_s
+        }
+    end
 
     before { host! 'api.taskmanager.dev' }
 
     describe 'GET users/:id' do
         before do
-            headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
             get "/users/#{user_id}", params: {}, headers: headers
         end
 
         context 'when the user exists' do
             it 'returns the user' do
-                user_response = JSON.parse(response.body, symbolize_names: true)
-                expect(user_response[:id]).to eq(user_id)
+                expect(json_body[:id]).to eq(user_id)
             end            
 
             it 'returns status code 200' do
                 expect(response).to have_http_status(200)
             end
         end
-
-        # context 'when the user exists' do
-        #     it 'returns the user' do
-        #         expect(json_body[:id]).to eq(user_id)
-        #     end
-        # end
     
         context 'when the user does not exist' do
             let(:user_id) { 10000 }        
@@ -41,8 +39,7 @@ RSpec.describe 'Users API', type: :request do
 
     describe 'POST /users' do
         before do
-            header = { 'Accept' => 'application/vnd.taskmanager.v1' }
-            post '/users', params: { user: user_params }, headers: header
+            post '/users', params: { user: user_params }.to_json, headers: headers
         end
 
         context 'when the request params is valid' do
@@ -53,8 +50,7 @@ RSpec.describe 'Users API', type: :request do
             end
 
             it 'returns json data for the created user' do
-                user_response = JSON.parse(response.body, symbolize_names: true)
-                expect(user_response[:email]).to eq(user_params[:email])
+                expect(json_body[:email]).to eq(user_params[:email])
             end
         end
         
@@ -66,9 +62,8 @@ RSpec.describe 'Users API', type: :request do
             end
 
             it 'returns json data for the errors' do
-                user_response = JSON.parse(response.body, symbolize_names: true)
 
-                expect(user_response).to have_key(:errors)
+                expect(json_body).to have_key(:errors)
             end
         end
 
@@ -76,8 +71,7 @@ RSpec.describe 'Users API', type: :request do
     
     describe 'PUT /users/:id' do
         before do
-            header = { 'Accept' => 'application/vnd.taskmanager.v1' }
-            put "/users/#{user_id}", params: { user: user_params }, headers: header
+            put "/users/#{user_id}", params: { user: user_params }.to_json, headers: headers
         end
 
         context 'when request params are valid' do
@@ -88,8 +82,7 @@ RSpec.describe 'Users API', type: :request do
             end
 
             it 'returns json data for the updated user' do
-                user_response = JSON.parse(response.body, symbolize_names: true)
-                expect(user_response[:email]).to eq(user_params[:email])
+                expect(json_body[:email]).to eq(user_params[:email])
             end
         end
 
@@ -101,15 +94,13 @@ RSpec.describe 'Users API', type: :request do
             end
 
             it 'returns json data for the errors' do
-                user_response = JSON.parse(response.body, symbolize_names: true)
-                expect(user_response).to have_key(:errors)
+                expect(json_body).to have_key(:errors)
             end
         end
     end
 
     describe 'DELETE /users/:id' do
         before do
-            headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
             delete "/users/#{user_id}", params: {}, headers: headers
             
             it 'returns status code 204' do
