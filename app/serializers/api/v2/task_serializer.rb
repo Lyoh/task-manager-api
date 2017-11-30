d99a42ee33673a85dd8adaaae192700ed6e9f4a1
Riubbe Oliveira <rb.oliveira@hotmail.com>
2017-11-30 00:09:10 +0000

    adding new I18n time format

--- app/serializers/api/v2/task_serializer.rb
+++ app/serializers/api/v2/task_serializer.rb
@@ -1,7 +1,7 @@
 class Api::V2::TaskSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :deadline, :done, 
              :user_id, :created_at, :updated_at,
-             :short_description, :is_late
+             :short_description, :is_late, :deadline_formatted
 
   def short_description
     object.short_description
@@ -9,6 +9,10 @@ class Api::V2::TaskSerializer < ActiveModel::Serializer
 
   def is_late
     object.late?
+  end
+
+  def deadline_formatted
+    I18n.l(object.deadline, format: :datetime) if object.deadline.present?
   end
 
   belongs_to :user
